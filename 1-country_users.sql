de44b8f5b1eae27e58522b91602f8928b90559e3
muluante <muluante@outlook.com>
2023-10-12 07:56:24 +0000

    1-country_users

--- 1-country_users.sql
+++ 1-country_users.sql
@@ -3,6 +3,7 @@
 --       email, string (255 characters), never null and unique
 --       name, string (255 characters)
 --       country, enumeration of countries: US, CO and TN, never null
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,

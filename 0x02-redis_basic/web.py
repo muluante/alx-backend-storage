14ede0495d17e6ab98a9c0b9436b05aa1bb72e43
muluante <muluante@outlook.com>
2023-10-18 10:15:24 +0000

    new update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """obtains the HTML content of a URL and returns it"""
-    redis_client = redis.Redis()
-    count_key = f"count:{url}"
-
-    if redis_client.exists(count_key):
-        redis_client.incr(count_key)
-        return redis_client.get(url).decode()
-
-    response = requests.get(url)
-    html_content = response.text
-
-    redis_client.set(url, html_content, ex=10)
-
-    redis_client.set(count_key, 1)
-
-    return html_content
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
